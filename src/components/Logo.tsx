105c811e1c8d363abc3205a4131bcf8a72d2ef38
Luc Hebert <luc.hebert13@gmail.com>
2024-04-18 01:58:28 -0400

    easter egg

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 import rnvLogo from "../assets/rnv-logo.svg";
 
 const Logo = () => {
+  const logoRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const logo = logoRef.current;
+
+    if (!logo) return;
+
+    const hoverAnimation = gsap.to(logo, {
+      rotation: 360,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+
+    const unhoverAnimation = gsap.to(logo, {
+      rotation: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+
+    const handleMouseEnter = () => {
+      hoverAnimation.restart();
+    };
+
+    const handleMouseLeave = () => {
+      unhoverAnimation.restart();
+    };
+
+    logo.addEventListener("mouseenter", handleMouseEnter);
+    logo.addEventListener("mouseleave", handleMouseLeave);
+
+    // Cleanup
+    return () => {
+      logo.removeEventListener("mouseenter", handleMouseEnter);
+      logo.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
   return (
     <div>
       <img
@@ -8,6 +47,7 @@ const Logo = () => {
         className="logo"
         alt="Retreats and Venues logo"
         aria-label="Retreats and Venues logo"
+        ref={logoRef}
       />
     </div>
   );
